2da3e2523948339ee1f3811302db9b797be5e081
santiagoandre <saramirez@unicauca.edu.co>
2019-06-24 17:03:49 -0700

    Se genero el html dinamico de el horario del profesor y se asociaron los bloques a la funcion setSchedule

--- src/app/components/profesor/choose-available-schedule/choose-available-schedule.component.ts
+++ src/app/components/profesor/choose-available-schedule/choose-available-schedule.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { NgModule } from '@angular/core';
 
 @Component({
-  selector: 'app-choose-available-schedule',
-  templateUrl: './choose-available-schedule.component.html',
-  styleUrls: ['./choose-available-schedule.component.css']
+	selector: 'app-choose-available-schedule',
+	templateUrl: './choose-available-schedule.component.html',
+	styleUrls: ['./choose-available-schedule.component.css']
 })
 export class ChooseAvailableScheduleComponent implements OnInit {
 
-  constructor() { }
+	user: any = {};
+	htmlSchedule: String = ``;
 
-  ngOnInit() {
-  }
+
+	constructor() {
+		this.loadSchedule();
+
+
+	}
+
+	ngOnInit() {
+	}
+	setSchedule(day,block){
+		console.log("elegir el bloque"+block+" del dia ",day)
+	}
+	loadSchedule() {
+		var days = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes"];
+		var blocks = 6;
+		for (var day in days) {
+			this.htmlSchedule += "<div class=day>\n";
+			this.htmlSchedule += '   <h3 class="dayText">' + days[day] + '</h3>\n';
+			for (var block = 0; block < blocks; block++) {
+				this.htmlSchedule += '   <div class="block" (click) = \'setSchedule("' + days[day] + '","' + block + '")\' height=20></div>\n';
+			}
+
+			this.htmlSchedule += "</div>\n";
+		}
+    console.log(this.htmlSchedule);
+	}
 
 }
