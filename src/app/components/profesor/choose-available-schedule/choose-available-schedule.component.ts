41fc7a1b8c50f5cfb08f9217c53cd63382dfc385
santiagoandre <saramirez@unicauca.edu.co>
2019-06-24 18:38:18 -0700

    Se implemento la logica para recolectar los horarios disponibles en un array, y la logica para cambiar el color de cada bloque cuando esta o no seleccionado

--- src/app/components/profesor/choose-available-schedule/choose-available-schedule.component.ts
+++ src/app/components/profesor/choose-available-schedule/choose-available-schedule.component.ts
@@ -10,7 +10,7 @@ export class ChooseAvailableScheduleComponent implements OnInit {
 
 	user: any = {};
 	htmlSchedule: String = ``;
-
+	schedule: any = [];
 
 	constructor() {
 		this.loadSchedule();
@@ -20,22 +20,56 @@ export class ChooseAvailableScheduleComponent implements OnInit {
 
 	ngOnInit() {
 	}
-	setSchedule(day,block){
-		console.log("elegir el bloque"+block+" del dia ",day)
+	setSchedule(day, block) {
+		console.log("elegir el bloque " + block + " del dia ", day);
+		var index = 0;
+		var found = false;
+		for (var s in this.schedule) {
+			console.log(this.schedule[s]);
+			//console.log(day + "==" + this.schedule[s][0] + " and " + this.schedule[s][1] + " == " + block);
+			if (this.schedule[s][0] == day && this.schedule[s][1] == block) {
+				found = true;
+				break;
+			}
+			index+=1;
+		}
+		if (found) {
+			console.log("found");
+			this.schedule.splice(index,1);
+		} else {
+			console.log("not found");
+			this.schedule.push([day, block]);
+		}
+
+		console.log(this.schedule);
 	}
 	loadSchedule() {
 		var days = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes"];
 		var blocks = 6;
 		for (var day in days) {
 			this.htmlSchedule += "<div class=day>\n";
-			this.htmlSchedule += '   <h3 class="dayText">' + days[day] + '</h3>\n';
+			this.htmlSchedule += '   <h3  class= "dayText">' + days[day] + '</h3>\n'
 			for (var block = 0; block < blocks; block++) {
-				this.htmlSchedule += '   <div class="block" (click) = \'setSchedule("' + days[day] + '","' + block + '")\' height=20></div>\n';
+				this.htmlSchedule += '   <div [ngClass] = \'getBlockClass(' + day + ',' + block + ')\' class="block" (click) = \'setSchedule(' + day + ',' + block + ')\' height=20></div>\n';
 			}
 
 			this.htmlSchedule += "</div>\n";
 		}
-    console.log(this.htmlSchedule);
+		console.log(this.htmlSchedule);
+	}
+	getBlockClass(day,block){
+		var found = false;
+		for (var s in this.schedule) {
+			console.log(this.schedule[s]);
+			//console.log(day + "==" + this.schedule[s][0] + " and " + this.schedule[s][1] + " == " + block);
+			if (this.schedule[s][0] == day && this.schedule[s][1] == block) {
+				found = true;
+				break;
+			}
+
+		}
+
+		return {select_block:found,unselect_block:!found};
 	}
 
 }
