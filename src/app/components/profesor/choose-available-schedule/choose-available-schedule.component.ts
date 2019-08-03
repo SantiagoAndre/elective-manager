859efb1c390877ad84e91bce568036a83b442253
santiagoandre <saramirez@unicauca.edu.co>
2019-08-02 18:03:31 -0700

    Se completo la comunicacion con el componente schedule, ademas se le anadio la logica de horario valido

--- src/app/components/profesor/choose-available-schedule/choose-available-schedule.component.ts
+++ src/app/components/profesor/choose-available-schedule/choose-available-schedule.component.ts
@@ -8,20 +8,27 @@ import {ScheduleComponent} from '../../schedule/schedule.component'
 	styleUrls: ['./choose-available-schedule.component.css']
 })
 export class ChooseAvailableScheduleComponent implements OnInit {
-
-	user: any = {};
-
+	days:any=[];
+	blocks:any=[];
+	validSchedule:any=[];
 	constructor(private sendSchedule: SendAvailableScheduleService) {
-@input() infohijo:any;
+
 
 
 	}
-
+recibirlista($event){
+	console.log(event);
+}
 	ngOnInit() {
+		this.loadSchedule();
 	}
 
 
-
+	loadSchedule() {
+		this.days = [ "Lunes", "Martes", "Miercoles", "Jueves","Viernes"];
+		this.blocks = ['7-9','9-11','11-1','1-2','2-4','4-6','6-8'];
+		this.validSchedule = [[0,0],[	1,1],[2,2],[4,4]];
+	}
 
 	save() {
 
--- src/app/components/schedule/schedule.component.ts
+++ src/app/components/schedule/schedule.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit,Input,Output } from '@angular/core';
+import { Component, OnInit,Input,Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-schedule',
@@ -6,48 +6,58 @@ import { Component, OnInit,Input,Output } from '@angular/core';
   styleUrls: ['./schedule.component.css']
 })
 export class ScheduleComponent implements OnInit {
+@Output() emitterSchedule: EventEmitter <any> = new EventEmitter <any> ();
+@Input() days : any;
+@Input() blocks: any;
+@Input() validSchedule: any;
+schedule:any;
 
-  schedule: any = [];
 
 //@Output outEvent = new EventEmitter<any>();
   constructor() { }
 
   ngOnInit() {
-    this.loadSchedule();
+    this.schedule=[];
   }
   sendSchedule(){
     //this.outEvent.emit(this.schedule);
   }
-  loadSchedule() {
-		this.days = [ "Lunes", "Martes", "Miercoles", "Jueves","Viernes"];
-		this.blocks = ['7-9','9-11','11-1',null,'2-4','4-6','6-8'];
 
-	}
+  save(){
+    console.log(this.days);
+    console.log(this.blocks);
+    //this.listah.emit(this.schedule);
+  }
+  find(day, block,schedule){
+    var index = 0;
+    for (var s in schedule) {
+      //console.log(day + "==" + this.schedule[s][0] + " and " + this.schedule[s][1] + " == " + block);
+      if (schedule[s][0] == day && schedule[s][1] == block) {
+        return  index;
+      }
+      index += 1;
+
+    }
+    return -1;
+  }
   getBlockClass(day, block) {
-		var found = false;
-		for (var s in this.schedule) {
-			//console.log(day + "==" + this.schedule[s][0] + " and " + this.schedule[s][1] + " == " + block);
-			if (this.schedule[s][0] == day && this.schedule[s][1] == block) {
-				found = true;
-				break;
-			}
-
-		}
-
-		return { select_block: found, unselect_block: !found };
+    var valid = this.find(day,block,this.validSchedule)!=-1;
+    var found = false;
+    if(valid){
+		    found = this.find(day,block,this.schedule)!=-1;
+    }
+		return {  valid_block: valid&&!found , invalid_block: !valid,select_block: found};
 	}
   setSchedule(day, block) {
     console.log("elegir el bloque " + block + " del dia ", day);
-    var index = 0;
-    var found = false;
-    for (var s in this.schedule) {
-      //console.log(day + "==" + this.schedule[s][0] + " and " + this.schedule[s][1] + " == " + block);
-      if (this.schedule[s][0] == day && this.schedule[s][1] == block) {
-        found = true;
-        break;
-      }
-      index += 1;
+
+    if(this.find(day,block,this.validSchedule)== -1){
+      console.log("Invalid");
+      return;
     }
+
+    var index = this.find(day,block,this.schedule);
+    var found = index!= -1;
     if (found) {
       console.log("found");
       this.schedule.splice(index, 1);
